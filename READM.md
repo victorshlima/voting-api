204596b081eae9679c1335ecaa35e84a71b5cc5b
Victor Hugo Soares Lima <victorshlima@gmail.com>
2026-02-08 12:31:42 -0300

    Add - Initial configuration Add - Posgres, jpa, and flyway properties add - Creating tables, agenda, vote_session, vote, associate Add - Resilience and consistency properties Add - Postgres Add - Readme.md

---
+++ READM.md
@@ -0,0 +1,7 @@
+
+# Spring Boot + Flyway + Docker (PostgreSQL)
+
+## 1) Run localy
+
+ - docker compose up -d
+
---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.10"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.hibernate.orm") version "6.6.41.Final"
+	id("org.graalvm.buildtools.native") version "0.10.6"
+}
+
+group = "com.challenge"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+hibernate {
+	enhancement {
+		enableAssociationManagement = true
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
